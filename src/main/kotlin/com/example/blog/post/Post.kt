1c1ab9c7f1d14691e2688fc095b2086762fa48aa
soonyubi <soonyubi1@naver.com>
2024-06-14 16:22:40 +0900

    feat: define comment and tag entity

--- src/main/kotlin/com/example/blog/post/Post.kt
+++ src/main/kotlin/com/example/blog/post/Post.kt
@@ -1,6 +1,7 @@
 package com.example.blog.post
 
 import com.example.blog.common.PrimaryKeyEntity
+import com.example.blog.tag.Tag
 import com.example.blog.user.User
 import jakarta.persistence.*
 import java.time.LocalDateTime
@@ -10,7 +11,8 @@ class Post(
     title: String,
     content: String,
     information: PostInformation,
-    user : User
+    user : User,
+    tags: Set<Tag>
 ) : PrimaryKeyEntity() {
     @Column(nullable = false)
     var createdAt : LocalDateTime = LocalDateTime.now()
@@ -29,6 +31,20 @@ class Post(
     var writer : User = user
         protected  set
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.PERSIST, CascadeType.MERGE])
+    @JoinTable(
+        name = "board_tag_assoc",
+        joinColumns = [JoinColumn(name = "post_id")],
+        inverseJoinColumns = [JoinColumn(name = "tag_id")]
+    )
+    protected var mutableTags: MutableSet<Tag> = tags.toMutableSet()
+    val tags: Set<Tag> get() = mutableTags.toSet()
+
+    @ElementCollection
+    @CollectionTable(name = "post_comment")
+    private val mutableComments : MutableList<Comment> = mutableListOf()
+    val comments: List<Comment> get() = mutableComments.toList()
+
     @Embedded
     var information : PostInformation = information
         protected set
@@ -37,6 +53,22 @@ class Post(
         this.title=data.title
         this.information=data.information
         this.content=data.content
+    }
+
+    fun addTag(tag : Tag){
+        mutableTags.add(tag)
+    }
+
+    fun removeTag(tagId: String){
+        mutableTags.removeIf { it.id == tagId }
+    }
+
+    fun addComment(comment : Comment){
+        mutableComments.add(comment)
+    }
+
+    fun removeComment(comment: Comment){
+        mutableComments.remove(comment)
     }
 
     init {
@@ -52,6 +84,19 @@ data class PostInformation(
     @Column(nullable = false)
     val ranking: Int
 )
+
+@Embeddable
+data class Comment(
+    @Column(name = "content", length = 3000)
+    private var _content : String,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "writer_id")
+    private var _writer : User
+){
+    val content : String get() = _content
+    val writer : User get() = _writer
+}
 
 data class PostUpdateData(
     val title:String,
